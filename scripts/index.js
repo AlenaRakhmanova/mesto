b2b6e9e293e8351ff65820d6876b2b11626ba745
Alena Rakhmanova <rakhmanova.ale@yandex.ru>
2023-01-31 02:47:07 +0300

    feat: добавить новый попап и карточки template

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,44 @@
 const profile = document.querySelector(".profile");
 const buttonEdit = profile.querySelector(".profile__button-edit");
 const popup = document.querySelector(".popup");
-const popupButtonClose = popup.querySelector(".popup__button-close");
+const popupEdit = document.querySelector(".popup_edit");
+const popupAdd = document.querySelector(".popup_add");
+const popupButtonClose = document.querySelectorAll(".popup__button-close");
 const profileFullName = profile.querySelector(".profile__full-name");
 const profileInf = profile.querySelector(".profile__information");
+const buttonAdd = profile.querySelector(".profile__button-add");
 const popupFieldName = popup.querySelector(".popup__field_value_name");
 const popupFieldInfo = popup.querySelector(".popup__field_value_info");
 const formEdit = popup.querySelector(".popup__form");
+let buttonLike;
+const template = document.querySelector("#places").content.querySelector(".elements__place");
+const elements = document.querySelector(".elements");
+const initialCards = [
+  {
+    name: "Гора Эльбрус",
+    link: "./images/place/elbrus.jpg",
+  },
+  {
+    name: "Алтай",
+    link: "./images/place/altay.jpg",
+  },
+  {
+    name: "Камчатский край",
+    link: "./images/place/kamchtka.jpg",
+  },
+  {
+    name: "Карелия",
+    link: "./images/place/kareliya.jpg",
+  },
+  {
+    name: "Карачаево-Черкесская Республика",
+    link: "./images/place/karachaevo.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "./images/place/baikal.jpg",
+  },
+];
 
 /*Функция открытия модального окна*/
 function showPopup(modal) {
@@ -14,18 +46,24 @@ function showPopup(modal) {
 }
 
 /*Функция закрытия модального окна*/
-function closePopup(modal) {
+function closePopup() {
+  popupButtonClose.forEach((button) => {
+    button.addEventListener("click", toClose);
+  });
+}
+
+function toClose(evt) {
+  let button = evt.target;
+  let modal = button.closest(".popup");
   modal.classList.remove("popup_opened");
 }
+
+/*------------------------------------*/
 
 function handleEditProfile() {
   popupFieldName.value = profileFullName.textContent;
   popupFieldInfo.value = profileInf.textContent;
-  showPopup(popup);
-}
-
-function handleCloseEdit() {
-  closePopup(popup);
+  showPopup(popupEdit);
 }
 
 function handleFormSubmit(evt) {
@@ -35,25 +73,51 @@ function handleFormSubmit(evt) {
   closePopup(popup);
 }
 
-// function toLike(evt) {
-//   let button = evt.target;
-//   if (button.classList.contains("button-like_active")) {
-//     button.classList.remove("button-like_active");
-//   } else {
-//     button.classList.add("button-like_active");
-//   }
-// }
+function toLike(evt) {
+  let button = evt.target;
+  if (button.classList.contains("elements__button-like_active")) {
+    button.classList.remove("elements__button-like_active");
+  } else {
+    button.classList.add("elements__button-like_active");
+  }
+}
 
-// function handleLikes() {
-//   buttonLike.forEach(function (button) {
-//     button.addEventListener("click", toLike);
-//   });
-// }
+function handleLikes() {
+  buttonLike.forEach(function (button) {
+    button.addEventListener("click", toLike);
+  });
+}
+
+/*Функция создания карточки*/
+function createCard(item) {
+  const card = template.cloneNode(true);
+  card.querySelector(".elements__image").src = item.link;
+  card.querySelector(".elements__image").alt = item.name;
+  card.querySelector(".elements__title").textContent = item.name;
+
+  return card;
+}
+
+function renderCards(items) {
+  const cards = items.map((item) => {
+    return createCard(item);
+  });
+  elements.prepend(...cards);
+  buttonLike = document.querySelectorAll(".elements__button-like");
+}
+
+function handleAddition() {
+  showPopup(popupAdd);
+}
+
+renderCards(initialCards);
 
 buttonEdit.addEventListener("click", handleEditProfile);
 
-popupButtonClose.addEventListener("click", handleCloseEdit);
+buttonAdd.addEventListener("click", handleAddition);
 
 formEdit.addEventListener("submit", handleFormSubmit);
 
-// handleLikes();
+handleLikes();
+
+closePopup();
